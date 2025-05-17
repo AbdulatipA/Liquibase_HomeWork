ff22252d3ae2f55786328647ad9b7ec890c75016
AbdulatipA <ataev0461@gmail.com>
2025-05-17 17:17:48 +0300

    Liquibase_HomeWork_1

---
+++ src/main/java/org/example/liquibase_homework/LiquibaseHomeWorkApplication.java
@@ -0,0 +1,13 @@
+package org.example.liquibase_homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquibaseHomeWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiquibaseHomeWorkApplication.class, args);
+    }
+
+}
